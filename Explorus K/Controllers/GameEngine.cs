525c89a43e63a66ecc652c033933d83bb76d83b9
Mathieu Cloutier <clom1811@usherbrooke.ca>
2022-08-31 14:36:06 -0400

    partie 14.4 du labo

---
+++ Explorus K/Controllers/GameEngine.cs
@@ -0,0 +1,30 @@
+﻿using Explorus_K.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Explorus_K.Controllers
+{
+    class GameEngine
+    {
+        private GameView oView;
+        public GameEngine()
+        {
+            oView = new GameView();
+            Thread thread = new Thread(new ThreadStart(GameLoop));
+            thread.Start();
+            oView.Show();
+        }
+
+        private void GameLoop()
+        {
+            oView.Render();
+            Thread.Sleep(5000);
+            oView.Close();
+        }
+
+    }
+}
---
+++ Explorus K/Views/GameView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Explorus_K.Views
+{
+    class GameView
+    {
+        public GameForm oGameForm;
+
+        public GameView()
+        {
+            oGameForm = new GameForm();
+            oGameForm.Paint += new PaintEventHandler(this.GameRenderer);
+        }
+
+        public void Show() 
+        {
+            Application.Run(oGameForm); 
+        }
+
+        public void Render()
+        {
+            if (oGameForm.Visible)
+                oGameForm.BeginInvoke((MethodInvoker)delegate {
+                    oGameForm.Refresh();
+                });
+        }
+
+        public void Close()
+        {
+            if (oGameForm.Visible)
+                oGameForm.BeginInvoke((MethodInvoker)delegate {
+                    oGameForm.Close();
+                });
+        }
+
+        private void GameRenderer(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.Black);
+            g.FillRectangle(new SolidBrush(Color.Yellow), 20, 20, 20, 20);
+        }
+    }
+}
