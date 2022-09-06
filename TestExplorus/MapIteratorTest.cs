c8fed324987d17f4d8be6e3cf877d92d270d7584
Tomy-Phillip <tomy-phillip@explor.ai>
2022-09-06 14:03:13 -0400

    Tests merge

--- TestExplorus/MapIteratorTest.cs
+++ TestExplorus/MapIteratorTest.cs
@@ -1,6 +1,7 @@
 ﻿using Explorus_K.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Drawing;
 
 namespace TestExplorus
 {
@@ -16,58 +17,58 @@ namespace TestExplorus
         public void givenWallToTheLeft_whenMovingLeft_thenShoudlNotBeAbleToMoveAndStayPut()
         {
             Iterator iterator = givenValidMapIterator();
-            int[] expectedPosition = iterator.Current();
+            Point expectedPosition = iterator.Current();
 
             bool result = iterator.isAbleToMoveLeft();
             iterator.MoveLeft();
 
             Assert.IsFalse(result);
-            CollectionAssert.AreEqual(expectedPosition, iterator.Current());
+            Assert.AreEqual(expectedPosition, iterator.Current());
         }
 
         [TestMethod]
         public void givenWallToTheRight_whenMovingRight_thenShoudlNotBeAbleToMoveAndStayPut()
         {
             Iterator iterator = givenValidMapIterator();
-            int[] expectedPosition = iterator.Current();
+            Point expectedPosition = iterator.Current();
 
             bool result = iterator.isAbleToMoveRight();
             iterator.MoveRight();
 
             Assert.IsFalse(result);
-            CollectionAssert.AreEqual(expectedPosition, iterator.Current());
+            Assert.AreEqual(expectedPosition, iterator.Current());
         }
 
         [TestMethod]
         public void givenWallBelow_whenMovingDown_thenShoudlNotBeAbleToMoveAndStayPut()
         {
             Iterator iterator = givenValidMapIterator();
-            int[] expectedPosition = iterator.Current();
+            Point expectedPosition = iterator.Current();
 
             bool result = iterator.isAbleToMoveDown();
             iterator.MoveDown();
 
             Assert.IsFalse(result);
-            CollectionAssert.AreEqual(expectedPosition, iterator.Current());
+            Assert.AreEqual(expectedPosition, iterator.Current());
         }
 
         [TestMethod]
         public void givenEmptySpaceAbove_whenMovingDUp_thenShoudlBeAbleToMoveAndUpdatePosition()
         {
             Iterator iterator = givenValidMapIterator();
-            int[] expectedPosition = {1, 0};
+            Point expectedPosition = new Point(1, 0);
 
             bool result = iterator.isAbleToMoveUp();
             iterator.MoveUp();
 
             Assert.IsTrue(result);
-            CollectionAssert.AreEqual(expectedPosition, iterator.Current());
+            Assert.AreEqual(expectedPosition, iterator.Current());
         }
 
 
         private Iterator givenValidMapIterator()
         {
-            return testMap.CreateIterator();
+            return testMap.CreateIterator("s");
         }
     }
 }
