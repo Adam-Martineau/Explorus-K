11ad674250e0f55c4f042fef6ff7eee47a13de87
Mathieu C <112578628+MardiDetente@users.noreply.github.com>
2022-09-16 18:15:41 -0400

    Added Player interface and subclasses for Slimus and ToxicSlime

--- Explorus K/Game/Constant.cs
+++ Explorus K/Game/Constant.cs
@@ -10,5 +10,9 @@ namespace Explorus_K.Game
     {
         public const int LARGE_SPRITE_DIMENSION = 52;
         public const int SMALL_SPRITE_DIMENSION = 26;
+
+        public const int SLIMUS_LIVES = 6;
+        public const int TOXIC_SLIME_LIVES = 2;
+        public const int INITIAL_BUBBLE_COUNT = 6;
     }
 }
