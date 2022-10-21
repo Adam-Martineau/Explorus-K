822298fada9ae9cdd34cdb85aa1fad2e9901364d
Tomy-Phillip <104791388+Tomy-Phillip@users.noreply.github.com>
2022-10-21 12:42:52 -0400

    Tests menu

--- TestExplorus/TestsController/MenuTests.cs
+++ TestExplorus/TestsController/MenuTests.cs
@@ -36,29 +36,152 @@ namespace TestExplorus.TestsController
         public void startGameTest()
         {
             MenuOptions menuOptions = oController.gameView.getMenuOptions();
-            Assert.AreEqual(menuOptions.getCurrentIndex(), 0);
+            Assert.AreEqual(0, menuOptions.getCurrentIndex());
             oController.gameView.selectMenu();
-            Assert.AreEqual(oController.State, GameState.RESUME);
+            Assert.AreEqual(GameState.RESUME, oController.State);
 
             Thread.Sleep(4000);
 
-            Assert.AreEqual(oController.State, GameState.PLAY);
+            Assert.AreEqual(GameState.PLAY, oController.State);
         }
 
         [TestMethod]
         public void stopGameTest()
         {
             MenuOptions menuOptions = oController.gameView.getMenuOptions();
-            Assert.AreEqual(menuOptions.getCurrentIndex(), 0);
+            Assert.AreEqual(0, menuOptions.getCurrentIndex());
             oController.gameView.cursorDown();
-            Assert.AreEqual(menuOptions.getCurrentIndex(), 1);
+            Assert.AreEqual(1, menuOptions.getCurrentIndex());
             oController.gameView.cursorDown();
-            Assert.AreEqual(menuOptions.getCurrentIndex(), 2);
+            Assert.AreEqual(2, menuOptions.getCurrentIndex());
             oController.gameView.cursorDown();
-            Assert.AreEqual(menuOptions.getCurrentIndex(), 3);
-            Assert.AreEqual(menuOptions.getCurrentType(), MenuCursor.EXIT_GAME);
+            Assert.AreEqual(3, menuOptions.getCurrentIndex());
+            Assert.AreEqual(MenuCursor.EXIT_GAME, menuOptions.getCurrentType());
             oController.gameView.selectMenu();
-            Assert.AreEqual(oController.State, GameState.STOP);
+            Assert.AreEqual(GameState.STOP, oController.State);
+        }
+
+        [TestMethod]
+        public void audioSelectTest()
+        {
+            MenuOptions menuOptions = oController.gameView.getMenuOptions();
+            Assert.AreEqual(0, menuOptions.getCurrentIndex());
+            oController.gameView.cursorDown();
+            Assert.AreEqual(1, menuOptions.getCurrentIndex());
+            oController.gameView.selectMenu();
+            menuOptions = oController.gameView.getMenuOptions();
+            Assert.AreEqual(MenuCursor.MUSIC_VOLUME, menuOptions.getCurrentType());
+        }
+
+        [TestMethod]
+        public void returnAudioTest()
+        {
+            oController.gameView.cursorDown();
+            oController.gameView.selectMenu();
+            MenuOptions menuOptions = oController.gameView.getMenuOptions();
+
+            Assert.AreEqual(MenuCursor.MUSIC_VOLUME, menuOptions.getCurrentType());
+            oController.gameView.cursorDown();
+            Assert.AreEqual(1, menuOptions.getCurrentIndex());
+            oController.gameView.cursorDown();
+            Assert.AreEqual(2, menuOptions.getCurrentIndex());
+            Assert.AreEqual(MenuCursor.RETURN, menuOptions.getCurrentType());
+            oController.gameView.selectMenu();
+            menuOptions = oController.gameView.getMenuOptions();
+            Assert.AreEqual(MenuCursor.START_GAME, menuOptions.getCurrentType());
+        }
+
+        [TestMethod]
+        public void musicVolumeTest()
+        {
+            oController.gameView.cursorDown();
+            oController.gameView.selectMenu();
+            MenuOptions menuOptions = oController.gameView.getMenuOptions();
+
+            Assert.AreEqual(MenuCursor.MUSIC_VOLUME, menuOptions.getCurrentType());
+            Assert.AreEqual(5, oController.MusicVolume);
+            oController.gameView.volumeUp();
+            Assert.AreEqual(6, oController.MusicVolume);
+            oController.gameView.volumeDown();
+            Assert.AreEqual(5, oController.MusicVolume);
+        }
+
+        [TestMethod]
+        public void musicMuteTest()
+        {
+            oController.gameView.cursorDown();
+            oController.gameView.selectMenu();
+            MenuOptions menuOptions = oController.gameView.getMenuOptions();
+
+            Assert.AreEqual(MenuCursor.MUSIC_VOLUME, menuOptions.getCurrentType());
+            Assert.AreEqual(5, oController.MusicVolume);
+            oController.gameView.volumeUp();
+            oController.gameView.volumeUp();
+            Assert.AreEqual(7, oController.MusicVolume);
+            oController.gameView.mutevolume();
+            Assert.IsTrue(oController.MuteMusic);
+            oController.gameView.mutevolume();
+            Assert.IsFalse(oController.MuteMusic);
+            Assert.AreEqual(7, oController.MusicVolume);
+        }
+
+        [TestMethod]
+        public void soundVolumeTest()
+        {
+            oController.gameView.cursorDown();
+            oController.gameView.selectMenu();
+            MenuOptions menuOptions = oController.gameView.getMenuOptions();
+
+            Assert.AreEqual(MenuCursor.MUSIC_VOLUME, menuOptions.getCurrentType());
+            oController.gameView.cursorDown();
+            Assert.AreEqual(MenuCursor.SOUND_VOLUME, menuOptions.getCurrentType());
+            Assert.AreEqual(50, oController.SoundVolume);
+            oController.gameView.volumeUp();
+            Assert.AreEqual(51, oController.SoundVolume);
+            oController.gameView.volumeDown();
+            Assert.AreEqual(50, oController.SoundVolume);
+        }
+
+        [TestMethod]
+        public void soundMuteTest()
+        {
+            oController.gameView.cursorDown();
+            oController.gameView.selectMenu();
+            MenuOptions menuOptions = oController.gameView.getMenuOptions();
+
+            Assert.AreEqual(MenuCursor.MUSIC_VOLUME, menuOptions.getCurrentType());
+            oController.gameView.cursorDown();
+            Assert.AreEqual(MenuCursor.SOUND_VOLUME, menuOptions.getCurrentType());
+            Assert.AreEqual(50, oController.SoundVolume);
+            oController.gameView.volumeUp();
+            oController.gameView.volumeUp();
+            Assert.AreEqual(52, oController.SoundVolume);
+            oController.gameView.mutevolume();
+            Assert.IsTrue(oController.MuteSound);
+            oController.gameView.mutevolume();
+            Assert.IsFalse(oController.MuteSound);
+            Assert.AreEqual(52, oController.SoundVolume);
+        }
+
+        [TestMethod]
+        public void difficultySelectTest()
+        {
+            MenuOptions menuOptions = oController.gameView.getMenuOptions();
+            Assert.AreEqual(0, menuOptions.getCurrentIndex());
+            oController.gameView.cursorDown();
+            Assert.AreEqual(1, menuOptions.getCurrentIndex());
+            oController.gameView.cursorDown();
+            Assert.AreEqual(2, menuOptions.getCurrentIndex());
+            Assert.AreEqual(MenuCursor.DIFFICULTY, menuOptions.getCurrentType());
+            Assert.AreEqual(Difficulties.NORMAL, oController.GameDifficulty.Difficulty);
+            oController.gameView.selectMenu();
+            Assert.AreEqual(Difficulties.EXPERT, oController.GameDifficulty.Difficulty);
+            oController.gameView.selectMenu();
+            Assert.AreEqual(Difficulties.IMPOSSIBLE, oController.GameDifficulty.Difficulty);
+            oController.gameView.selectMenu();
+            Assert.AreEqual(Difficulties.EASY, oController.GameDifficulty.Difficulty);
+            oController.gameView.selectMenu();
+            Assert.AreEqual(Difficulties.NORMAL, oController.GameDifficulty.Difficulty);
         }
     }
 }
